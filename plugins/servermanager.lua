efdb029af20555c570c5acd87472566cad9ad1bb
Maliv <ma.vafaei.2002@gmail.com>
2016-02-03 19:05:24 +0330

    Update and rename Servermanager.lua to servermanager.lua

--- plugins/servermanager.lua
+++ plugins/servermanager.lua
@@ -1,13 +1,14 @@
 local function run(msg, matches)
 local text = matches[2]
-if matches[1] == "saveplug" and is_sudo(msg) then
+if matches[1] == "saveplug" and is_sudo(msg)
+then
 
   return "Plugin Saved"
  end
 end
 
 return {
- description = "Filemanager plugin saver!",
+ description = "filemanager plugin saver!",
  usage = {
   "!saveplug [name][ext] [plugintxt] : save plugin",
 },
